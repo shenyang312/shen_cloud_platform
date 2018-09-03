c0f2763032ee0b10730cbf4e7fedf497e939a324
shenyang <shen.5109256@qq.com>
2018-09-03 15:28:15 +0800

    spirng cloud config 上传

--- customer/src/main/java/com/shen/customer/web/TestController.java
+++ customer/src/main/java/com/shen/customer/web/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @Value("${from}")
-    private String from = "111";
+    private String from;
 
     @RequestMapping("/from")
     public String from(){
