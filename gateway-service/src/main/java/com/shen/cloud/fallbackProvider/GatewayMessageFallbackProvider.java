6ad36fe69a898c6308bb67f590fbc52659d77f94
shenyang <shen.5109256@qq.com>
2018-09-11 17:17:27 +0800

    zull回滚提示类

---
+++ gateway-service/src/main/java/com/shen/cloud/fallbackProvider/GatewayMessageFallbackProvider.java
@@ -0,0 +1,59 @@
+package com.shen.cloud.fallbackProvider;
+
+import com.shen.cloud.entity.ZmResult;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class GatewayMessageFallbackProvider implements FallbackProvider{
+    @Override
+    public String getRoute() {
+        return "message";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse() {
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream(new ZmResult("11","message服务超时").toString().getBytes());
+            }
+
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+        };
+    }
+}
