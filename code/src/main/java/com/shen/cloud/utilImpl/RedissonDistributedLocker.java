d035016888c8ea836b23898d003a2a3d5b029938
shenyang <shen.5109256@qq.com>
2018-12-03 11:35:10 +0800

    新增分布式锁 redisson 启动exhibition 模块 访问http://localhost:7003/testlock?name=liang 间隔 三秒 http://localhost:7003/testlock?name=shen 访问 每次请求都是锁住的

---
+++ code/src/main/java/com/shen/cloud/utilImpl/RedissonDistributedLocker.java
@@ -0,0 +1,44 @@
+package com.shen.cloud.utilImpl;
+
+import com.shen.cloud.util.DistributedLocker;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Lock接口实现类
+ * @author ko
+ *
+ */
+public class RedissonDistributedLocker implements DistributedLocker {
+
+    private RedissonClient redissonClient;
+
+    @Override
+    public void lock(String lockKey) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock();
+    }
+
+    @Override
+    public void unlock(String lockKey) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.unlock();
+    }
+
+    @Override
+    public void lock(String lockKey, int leaseTime) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock(leaseTime, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void lock(String lockKey, TimeUnit unit ,int timeout) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock(timeout, unit);
+    }
+
+    public void setRedissonClient(RedissonClient redissonClient) {
+        this.redissonClient = redissonClient;
+    }
+}
--- exhibition/src/main/java/com/shen/cloud/controller/TestOne.java
+++ exhibition/src/main/java/com/shen/cloud/controller/TestOne.java
@@ -3,15 +3,19 @@ package com.shen.cloud.controller;
 import com.shen.cloud.QueueCode;
 import com.shen.cloud.entity.Deploy;
 import com.shen.cloud.service.DeployService;
+import com.shen.cloud.util.RedissLockUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 
 @RestController
@@ -24,6 +28,13 @@ public class TestOne {
 
     @Resource
     private DeployService deployService;
+
+    private final String _lock  = "_lock";
+
+    static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static String getCurrentDate(){
+        return sdf.format(new Date());
+    }
 
     @RequestMapping("appset")
     public String testOne(){
@@ -42,5 +53,29 @@ public class TestOne {
         logger.info("========<记录>++++++++=========");
         deployService.addDeploy(Deploy.builder().uuid(UUID.randomUUID().toString()).name("feignTest").build());
         return null;
+    }
+
+    @RequestMapping(value = "/testlock",method= RequestMethod.GET)
+    public String testlock(String name) {
+
+        new Thread(() -> {
+
+            RedissLockUtil.lock(_lock, TimeUnit.MINUTES, 10);
+
+            System.out.println(getCurrentDate()+" "+name+" begin...");
+            for (int i = 0; i < 20; i++) {
+                try {
+                    Thread.sleep(1000);
+                    System.out.println(getCurrentDate()+" "+name+" "+i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(getCurrentDate()+" "+name+" end...");
+
+            RedissLockUtil.unlock(_lock);
+        }).start();
+
+        return "testlock";
     }
 }
