71ba4265bb35e19f77dcf00fcb0baeec76f5695a
shenyang <shen.5109256@qq.com>
2018-12-18 09:42:12 +0800

    redisTemplate 提供 散列 设置方法

--- code/src/main/java/com/shen/cloud/util/RedisHelper.java
+++ code/src/main/java/com/shen/cloud/util/RedisHelper.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ import javax.annotation.Resource;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -81,5 +83,64 @@ public class RedisHelper {
      */
     public boolean exists(final String key) {
         return redisTemplate.hasKey(key);
+    }
+
+    /**
+     * @param key
+     * @param value
+     * @param expireTime 秒为单位
+     */
+    public boolean setHash(String key, Map<String,String> value, Long expireTime){
+        boolean result = false;
+        try {
+            redisTemplate.opsForHash().putAll(key,value);
+            if(expireTime != 0){
+                redisTemplate.expire(key, expireTime, TimeUnit.SECONDS);
+            }
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public boolean hashIncrement(String key,String field,Long num){
+        boolean result = false;
+        try {
+            HashOperations<String,String,Object> opsForHash = redisTemplate.opsForHash();
+            opsForHash.increment(key,field,num);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * @param key
+     */
+    public Map<String,String> getHash(String key){
+        return redisTemplate.opsForHash().entries(key);
+    }
+
+    public Object getHash(String key,String field){
+        return redisTemplate.opsForHash().get(key,field);
+    }
+
+    /**
+     * @param key
+     * @param expireTime 秒为单位
+     */
+    public boolean expireTime(String key,Long expireTime){
+        boolean result = false;
+        try {
+            if(expireTime != 0){
+                redisTemplate.expire(key, expireTime, TimeUnit.SECONDS);
+            }
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
